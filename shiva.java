a849010728c9d1f9f6245b89732831b30eabae2d
shivaprasaath09 <75976441+shivaprasaath09@users.noreply.github.com>
2022-01-18 14:56:49 +0530

    Add files via upload

---
+++ shiva.java
@@ -0,0 +1,85 @@
+
+package OnlineClasses;
+
+import java.util.Scanner;
+
+public class shiva 
+{
+	public static void main(String[] args) 
+	{
+		
+		//Q1  Implementing all the operators 
+		/*	//Assignment
+			int a = 16;
+				
+			//Ternary
+			String res = a%2 == 0?"Even":"Odd";
+			System.out.println(res);
+				
+			int a1  = 5;
+			//Bitwise
+			System.out.println(Integer.toBinaryString(a1));
+			System.out.println(Integer.toBinaryString(~a1));
+			System.out.println(~a); */
+		
+		
+		//Q2 check if a number is prime number or not
+		/* int num;
+		 Scanner sc = new Scanner(System.in);
+		 num = sc.nextInt();
+		 boolean flag = false;
+		 for (int i = 2; i <= num / 2; ++i) {
+		      if (num % i == 0) {
+		        flag = true;
+		        break;
+		      }
+		    }
+
+		    if (!flag)
+		      System.out.println(num + " is a prime number.");
+		    else
+		      System.out.println(num + " is not a prime number.");   */
+		
+		
+		
+		//Q3 check if a year is leap year or not 
+		  /*int year;
+	      Scanner sc = new Scanner(System.in);
+	      year = sc.nextInt();
+
+	      if (((year % 4 == 0) && (year % 100!= 0)) || (year%400 == 0))
+	         System.out.println(year + " year is a leap year");
+	      else
+	         System.out.println(year + " year is not a leap year"); */
+		
+		
+		
+		//Q4 sum of first 10 numbers
+		
+		/*int i, num = 10, sum = 0;  
+		for(i = 1; i <= num; ++i)  
+		{  
+		sum = sum + i;  
+		}     
+		System.out.println("sum of first 10 numbers is = " + sum);    */
+		
+		
+		//Q5 implement switch statement
+		 /* int number;  
+		  Scanner sc = new Scanner(System.in);
+	      number = sc.nextInt();
+		    switch(number)
+		    {    
+		    case 10: System.out.println("It is 10");  
+		    break;  
+		    case 20: System.out.println("It is 20");  
+		    break;  
+		    case 30: System.out.println("It is 30");  
+		    break;    
+		    default:System.out.println("None in 10,20 and 30");  
+		    }  */
+		
+
+	}
+
+}
